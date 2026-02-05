9d02e2e2e8ecf6e55445ce2a2554f8cc14452629
Sara <sara_castano88242@elpoli.edu.co>
2026-02-05 08:25:51 -0500

    Primer commit

---
+++ primeraClase.py
@@ -0,0 +1,7 @@
+class Estudiante:
+    def __init__(self, documento, nombre, telefono):
+        self.documento = documento
+        self.nombre = nombre
+        self.telefono = telefono
+
+estudiante1 = Estudiante(1036252584, "Sara", 3147249070
