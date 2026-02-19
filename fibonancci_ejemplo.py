e6f9caacb441a81c79bb4b69bfb4f12431ac11b6
Sara <sara_castano88242@elpoli.edu.co>
2026-02-19 09:02:20 -0500

    .

--- fibonancci_ejemplo.py
+++ fibonancci_ejemplo.py
@@ -1,8 +1,8 @@
-def fibonancci(n):
-    if n <= 1:
-        return n
+def fibonancci(n): # Función recursiva para calcular el número de Fibonacci
+    if n <= 1: # Caso base: si n es 0 o 1, el número de Fibonacci es igual a n
+        return n # Retorna n para los casos base
     else:
-        return fibonancci(n - 1) + fibonancci(n - 2)
+        return fibonancci(n - 1) + fibonancci(n - 2) # Llamada recursiva para calcular el número de Fibonacci
 # Ejemplo de uso    
 
 print(fibonancci(5))
