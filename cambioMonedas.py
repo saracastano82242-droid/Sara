d379da614a371a00fac0d193a2f97c984b44fa0e
Sara <sara_castano88242@elpoli.edu.co>
2026-02-26 07:53:44 -0500

    .

---
+++ cambioMonedas.py
@@ -0,0 +1,18 @@
+#ejemplo de clase de algoritmo de Fibonacci con lista
+def cambio(cantidad, monedas):
+    """Calcula el número mínimo de monedas necesarias para dar el cambio."""
+    if cantidad == 0: # Caso base: si la cantidad es 0, no se necesitan monedas
+        return 0
+    if cantidad < 0: # Caso base: si la cantidad es negativa, no es posible dar cambio
+        return float('inf') # No es posible dar cambio negativo
+    min_monedas = float('inf') # Inicializa el número mínimo de monedas como infinito
+
+    for moneda in monedas: # Itera sobre cada tipo de moneda disponibles
+        num_monedas = cambio(cantidad - moneda, monedas) # Llamada recursiva para calcular el número de monedas necesarias para la cantidad restante después de usar una moneda
+        if num_monedas != float('inf'): # Si el número de monedas no es infinito, actualiza el número mínimo de monedas
+            min_monedas = min(min_monedas, num_monedas + 1) # Compara el número mínimo de monedas actual con el número de monedas calculado en la llamada recursiva más una moneda adicional (la moneda que se acaba de usar)
+    return min_monedas # Retorna el número mínimo de monedas necesarias para dar el cambio de la cantidad dada con las monedas disponibles
+# Ejemplo de uso
+monedas = [1, 78, 20, 25] # Lista de monedas disponibles para dar el cambio
+cantidad = 6 # Cantidad para la cual se desea calcular el cambio
+print(cambio(cantidad, monedas)) # Imprime el número mínimo de monedas para dar el cambio de 6 con las monedas disponibles
