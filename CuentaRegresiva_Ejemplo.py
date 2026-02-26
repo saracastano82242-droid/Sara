99b52f887772504e251eb99a25f920719550284b
Sara <sara_castano88242@elpoli.edu.co>
2026-02-26 07:33:02 -0500

    .

---
+++ CuentaRegresiva_Ejemplo.py
@@ -0,0 +1,11 @@
+def cuenta_regresiva(n):
+#Imprime números de n hasta 1.
+
+    if n <= 0: # CASO BASE
+        print("¡Despegue!")
+        return
+    print(n) # Acción
+    cuenta_regresiva(n - 1) # CASO RECURSIVO (n se reduce)
+# Llamada
+cuenta_regresiva(5) 
+# Salida: 5, 4, 3, 2, 1, ¡Despegue!
